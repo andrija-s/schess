af84c90c54cdc176b41fa9ce47bddf7202c1b4bf
andrija-s <51809572+andrija-s@users.noreply.github.com>
2022-06-01 21:10:50 -0700

    test update

--- tests/test-PERFT.js
+++ tests/test-PERFT.js
@@ -23,7 +23,8 @@ function test(board, expected, depth) {
   console.log(result + "expected: " + expected + " result: " + nodes + " \n" + "depth: " +depth+"\n"+ "time: " + time + " sec\n" + board + "\n");
   assert.deepEqual(nodes, expected);
 }
-const DEPTH = 3
+const DEPTH = 4
+console.log("PERFT TESTS:")
 test("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 97862, DEPTH);
 test("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 43238, DEPTH + 1);
 test("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 9467, DEPTH);
