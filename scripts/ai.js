d73ab87300d28df851300849112129e0807a6ff6
andrija-s <51809572+andrija-s@users.noreply.github.com>
2022-06-05 20:07:36 -0700

    ai improved

--- scripts/ai.js
+++ scripts/ai.js
@@ -1,25 +1,79 @@
-import {Game, EMPTY, BISHOP, QUEEN, PAWN, KNIGHT, ROOK} from "./game.js";
+import {Game, WHITE, BLACK, KING, EMPTY, BISHOP, QUEEN, PAWN, KNIGHT, ROOK} from "./game.js";
+
+const PAWN_POS   = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                    0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+                    0.1, 0.1, 0.2, 0.3, 0.3, 0.2, 0.1, 0.1,
+                    .05, .05, 0.1, .25, .25, 0.1, .05, .05,
+                    0.0, 0.0, 0.0, 0.2, 0.2, 0.0, 0.0, 0.0,
+                    .05,-.05,-0.1, 0.0, 0.0,-0.1,-.05, .05,
+                    .05, 0.1, 0.1,-0.2,-0.2, 0.1, 0.1, .05,
+                    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+const KNIGHT_POS = [-0.5,-0.4,-0.3,-0.3,-0.3,-0.3,-0.4,-0.5,
+                    -0.4,-0.2, 0.0, 0.0, 0.0, 0.0,-0.2,-0.4,
+                    -0.3, 0.0, 0.1, .15, .15, 0.1, 0.0,-0.3,
+                    -0.3, .05, .15, 0.2, 0.2, .15, .05,-0.3,
+                    -0.3, 0.0, .15, 0.2, 0.2, .15, 0.0,-0.3,
+                    -0.3, .05, 0.1, .15, .15, 0.1, .05,-0.3,
+                    -0.4,-0.2, 0.0, .05, .05, 0.0,-0.2,-0.4,
+                    -0.5,-0.4,-0.3,-0.3,-0.3,-0.3,-0.4,-0.5];
+const BISHOP_POS = [-0.2,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.2,
+                    -0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.1,
+                    -0.1, 0.0, .05, 0.1, 0.1, .05, 0.0,-0.1,
+                    -0.1, .05, .05, 0.1, 0.1, .05, .05,-0.1,
+                    -0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0,-0.1,
+                    -0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1,-0.1,
+                    -0.1, .05, 0.0, 0.0, 0.0, 0.0, .05,-0.1,
+                    -0.2,-0.1,-0.1,-0.1,-0.1,-0.1,-0.1,-0.2];
+const ROOK_POS   = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                    .05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, .05,
+                   -.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-.05,
+                   -.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-.05,
+                   -.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-.05,
+                   -.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-.05,
+                   -.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-.05,
+                    0.0, 0.0, .05, .05, .05, 0.0, 0.0, 0.0];
+const QUEEN_POS = [-0.2,-0.1,-0.1,-.05,-.05,-0.1,-0.1,-0.2,
+                   -0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.1,
+                   -0.1, 0.0, .05, .05, .05, .05, 0.0,-0.1,
+                   -.05, 0.0, .05, .05, .05, .05, 0.0,-.05,
+                    0.0, 0.0, .05, .05, .05, .05, 0.0,-.05,
+                   -0.1, .05, .05, .05, .05, .05, 0.0,-0.1,
+                   -0.1, 0.0, .05, 0.0, 0.0, 0.0, 0.0,-0.1,
+                   -0.2,-0.1,-0.1,-.05,-.05,-0.1,-0.1,-0.2];
+const KING_POS  = [-0.3,-0.4,-0.4,-0.5,-0.5,-0.4,-0.4,-0.3,
+                   -0.3,-0.4,-0.4,-0.5,-0.5,-0.4,-0.4,-0.3,
+                   -0.3,-0.4,-0.4,-0.5,-0.5,-0.4,-0.4,-0.3,
+                   -0.3,-0.4,-0.4,-0.5,-0.5,-0.4,-0.4,-0.3,
+                   -0.2,-0.3,-0.3,-0.4,-0.4,-0.3,-0.3,-0.2,
+                   -0.1,-0.2,-0.2,-0.2,-0.2,-0.2,-0.2,-0.1,
+                    0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2,
+                    0.2, 0.3, 0.1, 0.0, 0.0, 0.1, 0.3, 0.2];
 function evaluateBoard (board, player) {
   let value = 0;
   for (let pos in board) {
     if (board[pos].TYPE===EMPTY) continue;
     let c = (board[pos].COLOR===player) ? 1 : -1;
+    let [x,y] = Game.nonlinear(pos);
+    [x, y] = (board[pos].COLOR===BLACK) ? [7-x,7-y] : [x,y];
+    let pos_eval = Game.linear(x,y);
     switch (board[pos].TYPE) {
       case BISHOP:
+        value += (3.0+BISHOP_POS[pos_eval]) * c;
+        break;
       case KNIGHT:
-        value += 3.0 * c;
+        value += (3.0+KNIGHT_POS[pos_eval]) * c;
         break;
       case QUEEN:
-        value += 9.0 * c;
+        value += (9.0+QUEEN_POS[pos_eval]) * c;
         break;
       case ROOK:
-        value += 5.0 * c;
+        value += (5.0+ROOK_POS[pos_eval]) * c;
         break;
       case PAWN:
-        let [x,y] = Game.nonlinear(pos);
-        if (c > 0) y = y / 28.0;
-        else y = (7.0 - y) / 28.0;
-        value += (1.0 + y) * c;
+        value += (1.0+PAWN_POS[pos_eval]) * c;
+        break;
+      case KING:
+        value += (KING_POS[pos_eval]) * c;
         break;
     }
   }
@@ -33,36 +87,40 @@ export function ai(depth, state, player, alpha=Number.NEGATIVE_INFINITY,beta=Num
     return [value, null]
   }
   let possibleMoves = state.allMoves();
-  let bestMove = (possibleMoves.length > 0) ? possibleMoves[0] : null;
-  let bestMoveValue = max_player ? Number.NEGATIVE_INFINITY
+  let best_move = (possibleMoves.length > 0) ? possibleMoves[0] : null;
+  let best_val = max_player ? Number.NEGATIVE_INFINITY
                                          : Number.POSITIVE_INFINITY;
   if (max_player) {
     for (let mov of possibleMoves) {
       state.move(mov);
-      let num = (mov.SPECIAL > 0) ? depth -3 : depth - 4;
+      let num = (mov.SPECIAL===QUEEN||mov.SPECIAL===ROOK) ? depth -3 : depth - 4;
       value = ai(num, state, player, alpha, beta, !max_player)[0];
       state.unmove()
-      if (value > bestMoveValue) {
-        bestMoveValue = value;
-        bestMove = mov;
+      if (value > best_val) {
+        best_val = value;
+        best_move = mov;
       }
-      if (bestMoveValue >= beta) break;
-      alpha = Math.max(alpha, bestMoveValue);
+      alpha = Math.max(alpha, best_val);
+      if (best_val >= beta) break;
     }
   }
   else {
     for (let mov of possibleMoves) {
       state.move(mov);
-      let num = (mov.SPECIAL > 0) ? depth -3 : depth - 4;
+      let num = (mov.SPECIAL===QUEEN||mov.SPECIAL===ROOK) ? depth -3 : depth - 4;
       value = ai(num, state, player, alpha, beta, !max_player)[0];
       state.unmove();
-      if (value < bestMoveValue) {
-        bestMoveValue = value;
-        bestMove = mov;
+      if (value < best_val) {
+        best_val = value;
+        best_move = mov;
       }
-      if (bestMoveValue <= alpha) break;
-      beta = Math.min(beta, bestMoveValue);
+      beta = Math.min(beta, best_val);
+      if (best_val <= alpha) break;
     }
   }
-  return [bestMoveValue, bestMove];
+  if (best_move===null) {
+    let color = (state.turn % 2===0) ? WHITE : BLACK;
+    if (!state.underAttack(color, state.king_positions[color])) best_val=0;
+  }
+  return [best_val, best_move];
 }
---
+++ tests/test-EXPERIMENTAL.js
@@ -0,0 +1,56 @@
+import {Chess} from "../scripts/chess.js";
+
+function PERFT(state, depth) {
+  let legal_moves = state.moves();
+  if (depth===1) {
+    return legal_moves.length;
+  }
+  let sum=0;
+  for (let mov of legal_moves) {
+    state.move(mov);
+    sum += PERFT(state, depth-1);
+    state.undo();
+  }
+  return sum;
+}
+function test(board, expected, depth) {
+  let test_state = new Chess(board);
+  console.log(board+"\ndepth: "+depth);
+  let time = Date.now();
+  let nodes;
+  nodes = PERFT(test_state, depth);
+  time = ((Date.now() - time) / 1000).toFixed(2);
+  let result = (expected===nodes) ? "\u2705 " : "\u274c ";
+  console.log("time: " + time + " sec\n" + "expected: " + expected + " result: " + nodes + " " +result +"\n");
+  return( expected===nodes)
+}
+let DEPTH = 3;
+if (process.argv.length > 2 && Number.isInteger(Number(process.argv[2])) 
+    && parseInt(process.argv[2]) < 5) {
+  DEPTH = parseInt(process.argv[2]);
+}
+const POSITIONS = [ 
+      { FEN: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 
+        NODES: [1, 20, 400, 8902, 197281] },
+      { FEN: "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -", 
+        NODES: [0, 14, 191, 2812, 43238] },
+      { FEN: "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 
+        NODES: [0, 48, 2039, 97862, 4085603] },
+      { FEN: "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 
+        NODES: [0, 6, 264, 9467, 422333] },
+      { FEN: "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        NODES: [0, 44, 1486, 62379, 2_103_487] },
+      { FEN: "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        NODES: [1, 46, 2079, 89890, 3_894_594] },
+];
+
+console.log("\n-- PERFT TESTS --\n")
+let count = 0;
+for (let pos of POSITIONS) {
+  let temp = test(pos.FEN, pos.NODES[DEPTH], DEPTH);
+  count += (temp) ? 1 : 0;
+}
+
+
+console.log(`${(count === POSITIONS.length ? "\u2705" : "\u274c")} ${count}/${POSITIONS.length} PERFT tests passed\n`);
+if (count !== POSITIONS.length) process.exit(1);
