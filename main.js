a3438196f825cb1688433d80b6a481c5a60be188
andrija-s <51809572+andrija-s@users.noreply.github.com>
2022-06-08 21:59:09 -0700

    evail var intro

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ let flipped = false;
 let promote_piece = Q_PROM;
 let c = null; // canvas element
 let ctx = null; // canvas context
+let evaluation = 0.0
 ////////////////////////////////////////////////////////////////////
 async function init() {
   c = document.createElement("canvas");
@@ -178,9 +179,9 @@ function move_ai(color) {
   let time = Date.now();
   let ai_move = ai(12, main_state, color);
   time = ((Date.now() - time) / 1000).toFixed(2);
-  
+  evaluation = ai_move[0].toFixed(2) * ((player===WHITE) ? -1 : 1);
   console.log("%f secs\neval: %f\nmove: %O\nleaf nodes:%i", 
-              time, ai_move[0].toFixed(2), ai_move[1], ai_move[2]);
+              time, evaluation, ai_move[1], ai_move[2]);
 
   if (ai_move[1] !== null) {
     main_state.move(ai_move[1]);
