a0e3802f608da022dc4af0a4638402928f366f01
andrija-s <andro.sunjic@gmail.com>
2022-06-20 21:10:57 +0200

    clean

--- rust-chess/src/lib.rs
+++ rust-chess/src/lib.rs
@@ -1,23 +1,20 @@
 use wasm_bindgen::prelude::*;
 use chess::{Piece, Board, File, ChessMove, Rank, CastleRights, BoardStatus, Square, Color, MoveGen, ALL_FILES, ALL_RANKS, ALL_SQUARES};
 use std::{mem, cmp, str::FromStr};
+
 #[wasm_bindgen]
 extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
+
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 
-    // The `console.log` is quite polymorphic, so we can bind it with multiple
-    // signatures. Note that we need to use `js_name` to ensure we always call
-    // `log` in JS.
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log_u32(a: u32);
 
-    // Multiple arguments too!
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log_many(a: &str, b: &str);
 }
+
 #[wasm_bindgen]
 pub fn init_moves(fen: &str) -> String {
   let board = Board::from_str(fen).expect("Valid FEN");
