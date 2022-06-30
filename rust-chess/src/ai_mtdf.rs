8e5f797b46c6fa173240f4efe72f479500cb2c28
andrija-s <andro.sunjic@gmail.com>
2022-07-01 01:21:43 +0200

    mtdf devolution

--- rust-chess/src/ai_mtdf.rs
+++ rust-chess/src/ai_mtdf.rs
@@ -1,5 +1,6 @@
 use chess::{Board, BoardStatus, ChessMove, Color, MoveGen, Piece};
-use std::{cmp, collections::HashMap, mem};
+use std::{cmp, collections::HashMap, mem, hash::BuildHasherDefault};
+use nohash_hasher::NoHashHasher;
 
 #[path = "./evaluation.rs"]
 mod evaluation;
@@ -8,24 +9,24 @@ struct Bounds {
   low: i32,
   high: i32,
   best: Option<ChessMove>,
-  order: [Piece; 6],
   depth: i32,
 }
 impl Bounds {
-  fn new(low: i32, high: i32, best: Option<ChessMove>, order: [Piece; 6], depth: i32) -> Bounds {
-    Bounds { low, high, best, order, depth }
+  fn new(low: i32, high: i32, best: Option<ChessMove>, depth: i32) -> Bounds {
+    Bounds { low, high, best, depth }
   }
 }
 
 const TOTAL_LIMIT: f64 = 5000.0;
-const ITER_LIMIT: f64 = 2000.0;
+const ITER_LIMIT: f64 = 1500.0;
 const CHECK_MATE: i32 = 10_000; // to help differentiate checkmates by depth
-const HASH_LIMIT: usize = 4_096_000;
-const HASH_CAP: usize = 1_024_000;
+const HASH_LIMIT: usize = 8_192_000;
+const HASH_CAP: usize = 8_192_000;
 
 // https://people.csail.mit.edu/plaat/mtdf.html
 pub fn ai(board: &Board) -> (i32, Option<ChessMove>) {
-  let mut table: HashMap<u64, Bounds> = HashMap::with_capacity(HASH_CAP);
+  let mut table: HashMap<u64, Bounds, BuildHasherDefault<NoHashHasher<u64>>> = 
+    HashMap::with_capacity_and_hasher(HASH_CAP, BuildHasherDefault::default());
   let mut result: (i32, Option<ChessMove>) = (0, None);
   let start_total: f64 = crate::now();
   let mut i: i32 = 1;
@@ -49,7 +50,7 @@ pub fn ai(board: &Board) -> (i32, Option<ChessMove>) {
   return (result.0, result.1);
 }
 
-fn mtdf(root_board: &Board, mut guess: i32, depth: i32, table: &mut HashMap<u64, Bounds>) -> (i32, Option<ChessMove>, bool) {
+fn mtdf(root_board: &Board, mut guess: i32, depth: i32, table: &mut HashMap<u64, Bounds, BuildHasherDefault<NoHashHasher<u64>>>) -> (i32, Option<ChessMove>, bool) {
   let mut best_move: Option<ChessMove> = None;
   let mut upperbound: i32 = i32::MAX;
   let mut lowerbound: i32 = i32::MIN;
@@ -80,7 +81,7 @@ fn get_ai_color(board: &Board, max_player: bool) -> Color {
 }
 
 fn ab_with_mem(
-  board: &Board, mut alpha: i32, mut beta: i32, depth: i32, table: &mut HashMap<u64, Bounds>, max_player: bool,
+  board: &Board, mut alpha: i32, mut beta: i32, depth: i32, table: &mut HashMap<u64, Bounds, BuildHasherDefault<NoHashHasher<u64>>>, max_player: bool,
 ) -> (i32, Option<ChessMove>) {
   match board.status() {
     BoardStatus::Stalemate => return (0, None),
@@ -89,8 +90,6 @@ fn ab_with_mem(
     _ => (),
   }
   let mut best_value: i32 = if max_player { i32::MIN } else { i32::MAX };
-  let mut order: [Piece; 6] = [Piece::Pawn, Piece::Knight, Piece::Bishop, Piece::Queen, Piece::Rook, Piece::King];
-  let mut piece_values: [i8; 6] = [0,0,0,0,0,0];
   let mut best_move: Option<ChessMove> = None;
 
   let entry: Option<&Bounds> = table.get(&board.get_hash());
@@ -107,7 +106,6 @@ fn ab_with_mem(
         alpha = cmp::max(alpha, t.low);
         beta = cmp::min(beta, t.high);
       }
-      order = t.order;
     }
     None => (),
   }
@@ -115,85 +113,65 @@ fn ab_with_mem(
     best_value = evaluation::evaluation(board, get_ai_color(board, max_player));
   } else if max_player {
     let mut a: i32 = alpha;
-    if best_value < beta {
-      let move_it: MoveGen = MoveGen::new_legal_ordered(board, &mut order);
-      let mut bresult = mem::MaybeUninit::<Board>::uninit();
-      for m in move_it {
-        match m.get_promotion() {
-          Some(Piece::Bishop) | Some(Piece::Rook) | Some(Piece::Knight) => continue,
-          _ => (),
-        }
-        unsafe {
-          board.make_move(m, &mut *bresult.as_mut_ptr());
-          let (value, _) = ab_with_mem(&*bresult.as_ptr(), a, beta, depth - 1, table, !max_player);
-          let piece_in: usize = board.piece_on(m.get_source()).unwrap().to_index();
-          if value > 0 {
-            piece_values[piece_in] += 1
-          }
-          else if value < 0 {
-            piece_values[piece_in] -= 1
-          }
-          if value > best_value {
-            best_value = value;
-            best_move = Some(m);
-          }
-          a = cmp::max(a, best_value);
-          if best_value >= beta {
-            break;
-          }
+    let mut move_it: MoveGen = MoveGen::new_legal(board);
+    let mut bresult = mem::MaybeUninit::<Board>::uninit();
+    if best_move.is_some() {
+      move_it.prioritize_move(best_move.unwrap());
+    }
+    for m in move_it {
+      match m.get_promotion() {
+        Some(Piece::Bishop) | Some(Piece::Rook) | Some(Piece::Knight) => continue,
+        _ => (),
+      }
+      unsafe {
+        board.make_move(m, &mut *bresult.as_mut_ptr());
+        let (value, _) = ab_with_mem(&*bresult.as_ptr(), a, beta, depth - 1, table, !max_player);
+        if value > best_value {
+          best_value = value;
+          best_move = Some(m);
         }
       }
+      a = cmp::max(a, best_value);
+      if best_value >= beta {
+          break;
+      }
     }
-    order.sort_by(
-      |a, b| 
-      piece_values[b.to_index()].partial_cmp(&piece_values[a.to_index()]).unwrap()
-    );
   } else {
     let mut b: i32 = beta;
-    if best_value > alpha {
-      let move_it: MoveGen = MoveGen::new_legal_ordered(board, &mut order);
-      let mut bresult = mem::MaybeUninit::<Board>::uninit();
-      for m in move_it {
-        match m.get_promotion() {
-          Some(Piece::Bishop) | Some(Piece::Rook) | Some(Piece::Knight) => continue,
-           _ => (),
-        }
-        unsafe {
-          board.make_move(m, &mut *bresult.as_mut_ptr());
-          let (value, _) = ab_with_mem(&*bresult.as_ptr(), alpha, b, depth - 1, table, !max_player);
-          let piece_in: usize = board.piece_on(m.get_source()).unwrap().to_index();
-          if value < 0 {
-            piece_values[piece_in] += 1
-          }
-          else if value > 0 {
-            piece_values[piece_in] -= 1
-          }
-          if value < best_value {
-            best_value = value;
-            best_move = Some(m);
-          }
-          b = cmp::min(b, best_value);
-          if best_value <= alpha {
-            break;
-          }
+    let mut move_it: MoveGen = MoveGen::new_legal(board);
+    let mut bresult = mem::MaybeUninit::<Board>::uninit();
+    if best_move.is_some() {
+      move_it.prioritize_move(best_move.unwrap());
+    }
+    for m in move_it {
+      match m.get_promotion() {
+        Some(Piece::Bishop) | Some(Piece::Rook) | Some(Piece::Knight) => continue,
+         _ => (),
+      }
+      unsafe {
+        board.make_move(m, &mut *bresult.as_mut_ptr());
+        let (value, _) = ab_with_mem(&*bresult.as_ptr(), alpha, b, depth - 1, table, !max_player);
+        if value < best_value {
+          best_value = value;
+          best_move = Some(m);
         }
       }
+      b = cmp::min(b, best_value);
+        if best_value <= alpha {
+          break;
+      }
     }
-    order.sort_by(
-      |a, b| 
-      piece_values[b.to_index()].partial_cmp(&piece_values[a.to_index()]).unwrap()
-    );
   }
-  if table.len() > HASH_LIMIT {
+  if table.len() >= HASH_LIMIT && !table.contains_key(&(*board).get_hash()) {
     return (best_value, best_move);
   }
   if best_value <= alpha {
-    table.insert((*board).get_hash(), Bounds::new(i32::MIN, best_value, best_move, order, depth));
+    table.insert((*board).get_hash(), Bounds::new(i32::MIN, best_value, best_move, depth));
   } else if best_value >= beta {
-    table.insert((*board).get_hash(), Bounds::new(best_value, i32::MAX, best_move, order, depth));
+    table.insert((*board).get_hash(), Bounds::new(best_value, i32::MAX, best_move, depth));
   } else {
     // best_value > alpha && best_value < beta
-    table.insert((*board).get_hash(), Bounds::new(best_value, best_value, best_move, order, depth));
+    table.insert((*board).get_hash(), Bounds::new(best_value, best_value, best_move, depth));
   }
 
   return (best_value, best_move);
